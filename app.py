3e691e83535209d1727e5bca1774f09c33c366a9
Samuel Cruz <contact@samcruz.dev>
2026-03-19 15:49:40 -0400

    Error semantico, oops

--- app.py
+++ app.py
@@ -6,20 +6,19 @@ def autenticar_usuario(username, password, ms):
     hardcoded_username = "admin"
     hardcoded_password = "1234"
 
-
-    result = { "success": False, "error_id": "", "response_time_ms": 0 }
+    result = {"success": False, "error_id": "", "response_time_ms": 0}
     result["response_time_ms"] = ms
 
-    if (username == "" or password == "")
+    if username == "" or password == "":
         result["error_id"] = "empty-fields"
 
         return result
 
-    if (username != hardcoded_username):
+    if username != hardcoded_username:
         result["error_id"] = "user-not-exist"
         return result
 
-    if (password != hardcoded_password):
+    if password != hardcoded_password:
         result["error_id"] = "incorrect-password"
         return result
 
